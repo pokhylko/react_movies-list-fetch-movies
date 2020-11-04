8af1ffea17b2bdf6da46dbfa4c33def7bc54398c
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-11-04 16:24:43 +0200

    Updates

---
+++ precache-manifest.d24b1aaf1d926b5b871333531eb286a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85c16139c2f1876958cc02126f3f88d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8ddc1fd7b8bf1f39578",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "300121e98dc51c3448d4",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "c8ddc1fd7b8bf1f39578",
+    "url": "./static/js/2.20a4f65f.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.20a4f65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300121e98dc51c3448d4",
+    "url": "./static/js/main.b6f60031.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
